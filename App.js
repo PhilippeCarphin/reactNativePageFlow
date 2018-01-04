13ff97e2818c7d1dcb5affed0674b03206b9e621
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-01-04 01:12:19 -0500

    Preparing for navigator (Tested)

--- App.js
+++ App.js
@@ -5,16 +5,29 @@
  */
 
 import React, { Component } from 'react';
+import { Navigator } from 'react-native'
 import PeopleIndexScreen from './app/screens/PeopleIndexScreen';
 
 export default class App extends Component<{}> {
+  _renderScene(route, navigator) {
+    var globalNavigatorProps = {
+      navigator: navigator
+    }
+
+    switch(route.ident) {
+      case "PeopleIndex":
+        return (<PeopleIndexScreen {...globalNavigatorProps} />)
+      default:
+        return (<PeopleIndexScreen {...globalNavigatorProps} />)
+    }
+  }
   render() {
-    return (
-      <PeopleIndexScreen />
-    )
+    return this._renderScene({ident: "PeopleIndex"}, {})
   }
 }
 
 const styles = {
+  navigatorStyle: {
 
+  }
 };
